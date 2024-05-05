ec3cf9ce70b1b584f8b659b9a3098f0a8b37bd18
Abdulirhman-Ashraf <153239328+Abdulirhman-Ashraf@users.noreply.github.com>
2024-05-05 17:54:13 +0300

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -1,12 +1,25 @@
-let card=document.querySelectorAll("#collection a")
-let btn=document.querySelector("#show")
+let card = document.querySelectorAll("#collection a");
+let btn = document.querySelector("#show");
+// navbar for mobile
+let btnMenu = document.querySelector(".menu");
 
+let uls = document.querySelector("ul");
 
-btn.addEventListener("click",function (){
-    for(let i=3;i<6;i++){
-        
-        card[i].classList.toggle("showMore")
-        card[i].classList.toggle("hidden")
-        
-    }
+btnMenu.addEventListener("click",function(){
+    uls.classList.toggle("toggleMeu")
 })
+
+btn.addEventListener("click", function show() {
+  if ((btn.innerHTML == "Show more")) {
+    btn.innerHTML = "Hide";
+  } 
+  else if ((btn.innerHTML == "Hide")) {
+    btn.innerHTML = "Show more";
+  }
+
+  for (let i = 3; i < 6; i++) {
+    card[i].classList.toggle("showMore");
+    card[i].classList.toggle("hidden");
+  }
+});
+
