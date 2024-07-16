eda08e562a974942e6a773e0f15f170feab7524a
Abdulirhman-Ashraf <153239328+Abdulirhman-Ashraf@users.noreply.github.com>
2024-07-16 04:43:53 +0300

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -8,7 +8,14 @@ let uls = document.querySelector("ul");
 btnMenu.addEventListener("click",function(){
     uls.classList.toggle("toggleMeu")
 })
+// textArea Limit Character
+let max=document.querySelector(".max");
+let textarea=document.querySelector("textarea");
+function textLimit(){
+  max.innerHTML =`Remaining Character : ${250-textarea.value.length}`;
+}
 
+// show more function
 btn.addEventListener("click", function show() {
   if ((btn.innerHTML == "Show more")) {
     btn.innerHTML = "Hide";
@@ -17,9 +24,8 @@ btn.addEventListener("click", function show() {
     btn.innerHTML = "Show more";
   }
 
-  for (let i = 3; i < 6; i++) {
+  for (let i = 3; i < card.length; i++) {
     card[i].classList.toggle("showMore");
     card[i].classList.toggle("hidden");
   }
 });
-
